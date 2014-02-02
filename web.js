cc32640f5e980853819b9d88de85141c492fd547
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2014-02-02 20:55:07 +0200

    a bit of styling and a public folder

--- web.js
+++ web.js
@@ -8,13 +8,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.set('layout', 'layout') 
 
-app.get('/', function(req, res){
-  res.render('index', { title: 'Welcome!'});
-});
+
+
+app.use(express.static(process.cwd() + '/public'));
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.send(500, 'Something broke!');
+});
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Welcome!'});
 });
 
 app.listen(process.env.port || 3000);
